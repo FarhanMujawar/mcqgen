eaa34054f9a12a23cb87f98c430cffc376109d27
FarhanMujawar <farhanmujawar0711@gmail.com>
2025-07-06 21:28:14 +0530

    File Updated

--- setup.py
+++ setup.py
@@ -1,2 +1,12 @@
 #for installing local package in my virtual environment
 
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='Farhan Mujawar',
+    author_email='farhanmujawar0711@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
